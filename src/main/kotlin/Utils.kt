4d33541de4b510420602719e33f94c1e6bd301d9
Steve McLaughlin <mclaughlinsteve90@gmail.com>
2018-10-09 17:51:07 -0400

    Added some config for the app

---
+++ src/main/kotlin/Utils.kt
@@ -0,0 +1,15 @@
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import java.io.FileInputStream
+
+fun loadConfig(): ConfigDto {
+    val mapper = ObjectMapper(YAMLFactory())
+    mapper.registerModule(KotlinModule())
+
+    return FileInputStream("${System.getProperty("user.dir")}/src/main/resources/config.yml").use {
+        mapper.readValue(it, ConfigDto::class.java)
+    }
+}
+
+data class ConfigDto(val basic: String, val repos: List<String>)
