1ddf8de8f49f55089418610220cc2b19288db7c3
Steve McLaughlin <mclaughlinsteve90@gmail.com>
2018-10-09 20:13:36 -0400

    Added function for removing labels from PR

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import com.github.kittinunf.fuel.core.FuelError
 import com.github.kittinunf.fuel.httpDelete
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.result.Result
@@ -8,6 +9,9 @@ import com.github.kittinunf.result.Result
 const val DELIMITER = "/"
 const val pullsEndpoint = "/pulls"
 const val labelsEndpoint = "/labels"
+const val issuesEndpoint = "/issues"
+
+const val LABEL = "Test"
 
 val config = loadConfig()
 val basic = config.basic
@@ -19,22 +23,33 @@ val mapper = jacksonObjectMapper()
 //TODO Re-implement using coroutines so we can hit multiple repositories at once
 fun main(args: Array<String>) {
     val pull = getOldestLabeledRequest()
+    //if (pull != null) deleteLabel(pull)
 }
 
 fun getOldestLabeledRequest(): Pull? {
-    val (_, _, result) = (repo + pullsEndpoint).httpGet().header(headers).responseString()
+    val url = repo + pullsEndpoint
+    val (_, _, result) = url.httpGet().header(headers).responseString()
     when (result) {
-        is Result.Failure -> {
-            val ex = result.getException()
-            println("======\n\n\n Something went wrong:\n $ex \n\n\n======")
-        }
+        is Result.Failure -> logFailure(result)
         is Result.Success -> {
             val pulls: List<Pull> = mapper.readValue(result.get())
 
-            return pulls.lastOrNull { p -> p.labels.isNotEmpty() && p.labels.any { l -> l.name == "Test" } }
+            return pulls.lastOrNull { p -> p.labels.isNotEmpty() && p.labels.any { l -> l.name == LABEL } }
         }
     }
     return null
+}
+
+private fun logFailure(result: Result.Failure<String, FuelError>) =
+        println("======\n\n\n Something went wrong:\n ${result.getException()} \n\n\n======")
+
+fun deleteLabel(pull: Pull) {
+    val url = repo + issuesEndpoint + DELIMITER + pull.number + labelsEndpoint + DELIMITER + LABEL
+    val (_, _, result) = url.httpDelete().header(headers).responseString()
+    when (result) {
+        is Result.Failure -> logFailure(result)
+        is Result.Success -> println("Successfully removed label")
+    }
 }
 
 
