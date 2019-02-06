718d9b491f7c44178b8728b0f2866efa046dbec9
Steve McLaughlin <mclaughlinsteve90@gmail.com>
2019-02-06 18:17:50 -0500

    Updated so that linter fails

--- src/main/kotlin/StatusService.kt
+++ src/main/kotlin/StatusService.kt
@@ -47,7 +47,7 @@ class StatusService(private val config: GithubConfig) {
             checks == null -> false
             status == null -> false
             else ->
-                if (status.values.map { statusState(it) }.plus(checks.values.map { checkState(it) }).any(::failure)) {
+                return if (status.values.map { statusState(it) }.plus(checks.values.map { checkState(it) }).any(::failure)) {
                     removeLabels(pull, LabelRemovalReason.OPTIONAL_CHECKS)
                 } else {
                     false
