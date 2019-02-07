3df05e4065ef17c1a6d0b977e075d1742231806a
Steve McLaughlin <mclaughlinsteve90@gmail.com>
2019-02-06 19:58:08 -0500

    Remove the delay completely

--- src/main/kotlin/Github.kt
+++ src/main/kotlin/Github.kt
@@ -129,7 +129,6 @@ class GithubService(private val config: GithubConfig) {
             is Result.Success -> {
                 logger.info { "Successfully merged ${pull.title}!" }
                 deleteBranch(pull)
-                Thread.sleep(10_000)
             }
         }
         return true
