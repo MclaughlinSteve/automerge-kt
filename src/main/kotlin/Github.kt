1d9060de5968fa8cc7b1403b5f290d709c55a139
Steve McLaughlin <mclaughlinsteve90@gmail.com>
2019-02-06 19:58:44 -0500

    Made delay 25 seconds just to see if it works...

--- src/main/kotlin/Github.kt
+++ src/main/kotlin/Github.kt
@@ -129,7 +129,7 @@ class GithubService(private val config: GithubConfig) {
             is Result.Success -> {
                 logger.info { "Successfully merged ${pull.title}!" }
                 deleteBranch(pull)
-                Thread.sleep(10_000)
+                Thread.sleep(25_000)
             }
         }
         return true
