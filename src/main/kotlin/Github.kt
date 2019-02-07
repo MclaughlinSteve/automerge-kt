09b8ee61d2b482d8dfd0f6ff30e89e9fd7fda63d
Steve McLaughlin <mclaughlinsteve90@gmail.com>
2019-02-06 19:40:28 -0500

    Longer delay...

--- src/main/kotlin/Github.kt
+++ src/main/kotlin/Github.kt
@@ -129,7 +129,7 @@ class GithubService(private val config: GithubConfig) {
             is Result.Success -> {
                 logger.info { "Successfully merged ${pull.title}!" }
                 deleteBranch(pull)
-                Thread.sleep(500)
+                Thread.sleep(2500)
             }
         }
         return true
